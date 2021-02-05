1c3ac6aeb4e40dd77b848261170e617036c40116
luiz123o <luiz123o@hotmail.com>
2021-02-05 10:56:50 -0300

    :tada: Feat/Start Project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import GlobalStyle from './styles/Global';
+import Home from './pages/Home';
+
+const App: React.FC = () => (
+  <>
+    <Home />
+    <GlobalStyle />
+  </>
+);
+export default App;
